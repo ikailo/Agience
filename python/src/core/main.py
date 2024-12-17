abeadbc2787ae746a3f35ad0140293da2e1a567d
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-12-17 21:12:05 +0530

    Main Script

--- python/src/core/main.py
+++ python/src/core/main.py
@@ -1,40 +1,53 @@
+import os
+from dotenv import load_dotenv
 import asyncio
-# from authority import Authority
 from broker import Broker
-# from host import Host
-# from agent import Agent
+from host import Host
 
-token = ""
+HOST_ID = os.getenv('HOST_ID')
+HOST_SECRET = os.getenv('HOST_SECRET')
+BROKER_URI = os.getenv('BROKER_URI')
 
 
 async def main():
+    # Initialize the Broker
     broker = Broker(None)
 
-    await broker.connect(token, "")
+    # Initialize Host with ID and Secret
+    # TODO: Need to initialize Authority and Agent Factory before the host
+    host = Host(host_id=HOST_ID, host_secret=HOST_SECRET, authority=None,
+                broker=broker, agent_factory=None, logger=None)
 
-    # authority = Authority(
-    #     authority_uri="https://auth.example.com",
-    #     broker=broker,
-    #     authority_data_adapter=MockAuthorityDataAdapter(),
-    # )
-    # await authority.initialize_with_backoff()
+    # Get the access token for the host
+    host_access_token = host.get_access_token()
 
-    # host = Host(
-    #     host_id="host123",
-    #     host_secret="supersecret",
-    #     authority=authority,
-    #     broker=broker,
-    #     agent_factory=MockAgentFactory(),
-    # )
-    # await host.start()
+    # Connect to the broker using the access token
+    await broker.connect(host_access_token, BROKER_URI)
 
-    # agent = Agent(
-    #     id="agent001",
-    #     name="MyAgent",
-    #     authority=authority,
-    #     broker=broker,
-    #     plugins=[MockPlugin()],
-    # )
-    # await agent.connect()
 
 asyncio.run(main())
+
+# authority = Authority(
+#     authority_uri="https://auth.example.com",
+#     broker=broker,
+#     authority_data_adapter=MockAuthorityDataAdapter(),
+# )
+# await authority.initialize_with_backoff()
+
+# host = Host(
+#     host_id="host123",
+#     host_secret="supersecret",
+#     authority=authority,
+#     broker=broker,
+#     agent_factory=MockAgentFactory(),
+# )
+# await host.start()
+
+# agent = Agent(
+#     id="agent001",
+#     name="MyAgent",
+#     authority=authority,
+#     broker=broker,
+#     plugins=[MockPlugin()],
+# )
+# await agent.connect()
