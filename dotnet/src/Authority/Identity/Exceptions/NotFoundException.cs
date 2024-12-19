532d715f2d4e40926fe3b7299690f800ccca1893
john-s4d <john@ikailo.com>
2024-12-19 11:50:38 -0500

    deletes

--- dotnet/src/Authority/Identity/Exceptions/NotFoundException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Agience.Authority.Identity.Exceptions
-{
-    public class NotFoundException : Exception
-    {
-        public NotFoundException(string message) : base(message) { }
-    }
-}
