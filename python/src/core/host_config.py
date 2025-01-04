f5d80bd6c0e0b9756af0cb72bc37236bd69cfcc4
Prathamesh Mutkure <pmutkure009@gmail.com>
2025-01-05 01:14:52 +0530

    Host Config anf Message ACL Checker

--- python/src/core/host_config.py
+++ python/src/core/host_config.py
@@ -1,9 +1,13 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
 from typing import Optional
 
 
-@dataclass
-class HostConfig:
+class HostConfig(BaseModel):
+    authority_uri_internal: Optional[str] = None
+    broker_uri_internal: Optional[str] = None
     authority_uri: Optional[str] = None
     host_id: Optional[str] = None
     host_secret: Optional[str] = None
+
+    class Config:
+        allow_population_by_field_name = True
