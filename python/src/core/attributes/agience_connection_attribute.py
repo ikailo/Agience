efeb285d1c8fc2853fe601a68378dfa90c4ef64a
Prathamesh Mutkure <pmutkure009@gmail.com>
2025-01-08 18:53:44 +0530

    Connection Attribute

---
+++ python/src/core/attributes/agience_connection_attribute.py
@@ -0,0 +1,29 @@
+from typing import Callable, TypeVar, Any
+from functools import wraps
+
+T = TypeVar('T', bound=Callable[..., Any])
+
+
+class AgienceConnection:
+    """
+    A decorator for marking methods as Agience connections.
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __call__(self, func: T) -> T:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            return func(*args, **kwargs)
+
+        # Store the connection name as metadata on the function
+        wrapper.__agience_connection_name__ = self.name
+        return wrapper  # type: ignore
+
+# Example usage:
+#
+# class MyClass:
+#     @AgienceConnection("connection_name")
+#     def my_method(self):
+#         pass
