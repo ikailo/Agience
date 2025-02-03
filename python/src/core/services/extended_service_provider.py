36d533d0a426b66919cb0fe7b99eb6b5de087476
Prathamesh Mutkure <pmutkure009@gmail.com>
2025-02-04 03:24:20 +0530

    Extended Service Provider

--- python/src/core/services/extended_service_provider.py
+++ python/src/core/services/extended_service_provider.py
@@ -1,1 +1,20 @@
-# TODO: Implement this after figuring out how to get the service provider in python
+from typing import Type, Any
+from utils.service_container import ServiceCollection, ServiceProvider, ServiceNotFoundError
+
+
+class ExtendedServiceProvider:
+    def __init__(self, provider: 'ServiceProvider'):
+        self._provider = provider
+        self._collection = ServiceCollection()
+        self._collection.add_scoped('ServiceProvider', lambda sp: self)
+
+    @property
+    def services(self) -> ServiceCollection:
+        return self._collection
+
+    def get_service(self, service_type: Type) -> Any:
+        local_provider = self._collection.build()
+        try:
+            return local_provider.get_service(service_type) or self._provider.get_service(service_type)
+        except ServiceNotFoundError:
+            return self._provider.get_service(service_type)
