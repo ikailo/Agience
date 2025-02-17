9696093644c786289a81b0fa9d3ff4407e64eb12
john-s4d <john@ikailo.com>
2025-02-17 09:38:55 -0500

    broker build

--- core/authority/mq-broker/entrypoint.sh
+++ core/authority/mq-broker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Use envsubst to replace variables in mosquitto.conf.template
-envsubst <  /etc/mosquitto/mosquitto.conf.template > /etc/mosquitto/mosquitto.conf
+envsubst < /etc/mosquitto/mosquitto.conf.template > /etc/mosquitto/mosquitto.conf
 
 # Start the main process
 exec "$@"
